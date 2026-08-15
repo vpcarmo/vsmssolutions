486593cfa9d7df182c903b736f7af239324113b5
suanecapinam <sucapinam@gmail.com>
2026-08-15 19:34:48 +0000

    fix: add permissão onrender

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,12 @@ export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: { entry: "server" },
+    server: { 
+      allowedHosts: [
+      'vsmssolutions.onrender.com'
+    ],
+      entry: "server"
+     },
   },
   nitro: {
     preset: "vercel",
