b6dad068f578157696df6135d48224fa965b4936
suanecapinam <sucapinam@gmail.com>
2026-08-15 19:52:33 +0000

    fix: add permissão vsms.com.br

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     server: {
-      allowedHosts: ["vsmssolutions.onrender.com"],
+      allowedHosts: ["vsmssolutions.onrender.com", "vsms.com.br"],
     },
   },
 
